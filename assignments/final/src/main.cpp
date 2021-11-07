ca0ca3a2ab5b74b686169b1fea6c3b228431fc29
AKadic-edu <akadic1@avans.nl>
2021-11-07 19:51:53 +0100

    Camera pan setup

--- assignments/final/src/main.cpp
+++ assignments/final/src/main.cpp
@@ -14,48 +14,49 @@ int main()
 
 	ml::Vector<float, 2> camera { 0.0f, 0.0f };
 
-	ml::Vector<float, 2> h1 { -1.0f,  0.0f };
-	ml::Vector<float, 2> h2 {  1.0f,  0.0f };
-	ml::Vector<float, 2> v1 {  0.0f, -1.0f };
-	ml::Vector<float, 2> v2 {  0.0f,  1.0f };
+	std::vector<ml::Vector<float, 2>> vertices { 2 };
 
-	std::vector<ml::Vector<float, 2>> vertices {
-		{ -1.0f,  0.0f },
-		{  1.0f,  0.0f },
-		{  0.0f, -1.0f },
-		{  0.0f,  1.0f }
-	};
-
-	vl::Line h { h1[0], h1[1], h2[0], h2[1] };
-	vl::Line v { v1[0], v1[1], v2[0], v2[1] };
-
-	float speed = 0.1;
+	float speed = 1.0f;
+	float width = 10.0f;
+	float height = 10.0f;
 
 	instance.onKeyDown([&](vl::Key k) {
 		if (k == vl::Key::l) camera[0] += speed;
 		if (k == vl::Key::h) camera[0] -= speed;
 		if (k == vl::Key::j) camera[1] -= speed;
 		if (k == vl::Key::k) camera[1] += speed;
+	});
 
-		std::cout << camera[0] << ", " << camera[1] << std::endl;
+	ml::Vector<float, 2> dragOrigin;
+
+	instance.onMouseDown([&](float x, float y) {
+		dragOrigin = { x, y };
+	});
+
+	instance.onMouseHold([&](float x, float y) {
+		camera[0] = -(x - dragOrigin[0]) * width;
+		camera[1] = -(y - dragOrigin[1]) * height;
 	});
 
 	return instance.run([&](vl::Renderer& r) {
 		r.clear();
 
-		auto view = ml::identity<float, 3, 3>();
-		view[2][0] = -camera[0];
-		view[2][1] = -camera[1];
+		auto translation = ml::identity<float, 3, 3>();
+		translation[2][0] = -camera[0];
+		translation[2][1] = -camera[1];
+		auto scale = ml::identity<float, 3, 3>();
+		scale[0][0] = 1.0f / width;
+		scale[1][1] = 1.0f / height;
 
 		std::vector<ml::Vector<float, 2>> tmp { vertices };
 		for (auto& v : tmp) {
-			auto u = view * ml::Vector<float, 3> { v[0], v[1], 1.0f };
+			auto u = scale * translation * ml::Vector<float, 3> { v[0], v[1], 1.0f };
 
 			v = { u[0], u[1] };
 		}
 
 		r.color(255, 255, 255);
 		r.drawLine({ -1.0f, tmp[0][1], 1.0f, tmp[1][1] });
-		r.drawLine({ tmp[2][0], -1.0f, tmp[3][0], 1.0f });
+		r.drawLine({ tmp[0][0], -1.0f, tmp[1][0], 1.0f });
 	});
 }
